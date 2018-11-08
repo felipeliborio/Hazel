de1dccd8b5571778e3e31957a8936cfbcf699ac0
Felipe Libório <felipeliborio@outlook.com>
2018-11-07 22:25:35 -0300

    Set up basic application and entry point.

---
+++ Hazel/src/Hazel/EntryPoint.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifdef HZ_PLATFORM_WINDOWS
+
+extern Hazel::Application* Hazel::CreateApplication();
+
+int main(int argc, char** argv)
+{
+	printf("Hello, Hazel!");
+	auto app = Hazel::CreateApplication();
+	app->Run();
+
+
+	delete app;
+}
+
+#endif
