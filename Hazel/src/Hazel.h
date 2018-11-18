e4bfd4c8ac27a01ba741f78fefd19dc0b45488a0
Felipe Libório <felipeliborio@outlook.com>
2018-11-18 15:54:56 -0300

    Using premake to generate the project files and set up the builds.

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -1,10 +1,10 @@
 #pragma once
 
-//For use by Hazel applications
-
 #include <stdio.h>
 
+//For use by Hazel applications
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 // ---- Entry Point -----------------------------------------------------------
 #include "Hazel/EntryPoint.h"
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,10 +6,13 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("Hello, Hazel!");
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized log.");
+	HZ_INFO("Initialized log.");
+	HZ_CRITICAL("Initialized log.");
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
-
 
 	delete app;
 }
